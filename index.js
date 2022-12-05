ae6945d0d186423f3c4c3e3da464a6337ea8fb0a
gitYoungren <youngrengitonga@gmail.com>
2022-12-05 22:40:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,55 +1,25 @@
-// function fetchBooks() {
-//   return fetch("https://anapioficeandfire.com/api/books")
-//   .then((resp) => resp.json())
-//   .then((json) => console.log(json));
-//   // To pass the tests, don't forget to return your fetch!
-  
-// }
+// write your code here
 
-// function renderBooks(books) {
-//   const main = document.querySelector('main');
-//   books.forEach(book => {
-//     const h2 = document.createElement('h2');
-//     h2.innerHTML = book.name;
-//     main.appendChild(h2);
-//   });
-// }
+function fetchBooks() {
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   fetchBooks();
-// });
-// AssertionError expected { Spy 'object.renderBooks' }
-// function renderBooks(books) {
-//   var main = document.querySelector('main');
-//   books.forEach(function (book) {
-//     var h2 = document.createElement('h2');
-//     h2.innerHTML = book.name;
-//     main.appendChild(h2);
-//   });
-// }
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
-document.addEventListener("DOMContentLoaded", function() {
-  fetchBooks();
-});
- function fetchBooks() {
-
-  fetch("https://anapioficeandfire.com/api/books")
-    .then(res => res.json()).then(json => renderBooks(json));
+  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(renderBookData =>renderBooks(renderBookData))
 }
+fetchBooks()
 
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  const main = document.querySelector("main");
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-    const h2 = document.createElement("h2");
-    h2.innerHTML = `<h2>${book.name}</h2>`;
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
     main.appendChild(h2);
-  };
+  });
+}
+renderBooks()
 
-
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+  return fetchBooks()
+});
