a4273f045baeb7b0bd03cede5c1ab15c9e660a99
gitYoungren <youngrengitonga@gmail.com>
2022-12-05 12:39:42 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//   .then((resp) => resp.json())
+//   .then((json) => console.log(json));
+//   // To pass the tests, don't forget to return your fetch!
   
-}
+// }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
 
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// });
+// AssertionError expected { Spy 'object.renderBooks' }
+// function renderBooks(books) {
+//   var main = document.querySelector('main');
+//   books.forEach(function (book) {
+//     var h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
 document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
+document.addEventListener("DOMContentLoaded", function() {
   fetchBooks();
 });
+ function fetchBooks() {
+
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json()).then(json => renderBooks(json));
+}
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  const main = document.querySelector("main");
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  };
+
+
